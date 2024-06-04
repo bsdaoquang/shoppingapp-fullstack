2baaf1566ef5d01fa24d4510ef05cc6ee65c9b46
bsdaoquang <bsdaoquang@gmail.com>
2024-06-04 22:58:56 +0700

    rename app

--- android/app/src/main/java/com/bsdaoquang/shoppingapp/MainApplication.kt
+++ android/app/src/main/java/com/bsdaoquang/shoppingapp/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.shoppingapp
+package com.bsdaoquang.shoppingapp
 
 import android.app.Application
 import com.facebook.react.PackageList
--- ios/RhinoTests/RhinoTests.m
+++ ios/RhinoTests/RhinoTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface shoppingappTests : XCTestCase
+@interface RhinoTests : XCTestCase
 
 @end
 
-@implementation shoppingappTests
+@implementation RhinoTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
